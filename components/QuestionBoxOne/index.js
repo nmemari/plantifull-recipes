544b8970d23157a10c3ca28dec614d5b80b8ab0e
Navid Memari <work.nmemari@gmail.com>
2023-04-08 15:06:45 -0700

    completed quiz elements

--- components/QuestionBoxOne/index.js
+++ components/QuestionBoxOne/index.js
@@ -1,16 +1,45 @@
 import styles from './QuestionBoxOne.module.css'
 import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import { questions } from '@/data/questions.js'
 
 export default function QuestionBoxOne() {
     const [answer, setAnswer] = useState('')
-    const [isActive, setIsActive] = useState(false);
+    const [isActive, setIsActive] = useState(true);
+    const [data, setData] = useState(questions);
     
     const handleAnswer = (event) => {
         setAnswer(event.target.value);
     }
 
+    const handleLableOne = () => {
+        if(answer === 'vegan'){
+            return 'var(--fern-green)';
+        }
+    }
+
+    const handleLableTwo = () => {
+        if(answer === 'pescatarian'){
+            return 'var(--fern-green)';
+        }
+    }
+
+    const handleLableThree = () => {
+        if(answer === 'ovo vegetarian'){
+            return 'var(--fern-green)';
+        }
+    }
+
+    const handleLableFour = () => {
+        if(answer === 'ovo-lacto vegetarian'){
+            return 'var(--fern-green)';
+        }
+    }
+
     return (
-        <>
+        <div className={styles.container} style={{
+            display: isActive ? 'flex' : 'none'
+        }}>
             <div className={styles.progressBar}>
                 <div className={styles.progressDone}></div>
                 <div className={styles.progress}></div>
@@ -18,10 +47,13 @@ export default function QuestionBoxOne() {
             </div>
 
             <div className={styles.quizContainer}>
-                <h1 className={styles.question}>Which type do you practice?</h1>
+                <h1 className={styles.question}>{data[0].question}</h1>
                 <div className={styles.answerCont}>
-                    <div className={styles.answerBox}>
+                    <div className={styles.answerBox}  style={{
+                            backgroundColor: handleLableOne()
+                        }}>
                         <label className={styles.answer}>
+                            <Image src={data[0].img[0]} width={125} height={125}/>
                             <input
                                 type="radio"
                                 name="answer"
@@ -29,43 +61,57 @@ export default function QuestionBoxOne() {
                                 checked={answer === 'vegan'}
                                 onChange={handleAnswer}
                             />
-                            <h3>Vegan</h3>
+                            <h3>{data[0].options[0]}</h3>
                         </label>
                     </div>
-                    
-                    <label className={styles.answer}>
-                        <input
-                            type="radio"
-                            name="answer"
-                            value="pescatarian"
-                            checked={answer === 'pescatarian'}
-                            onChange={handleAnswer}
-                        />
-                        <h3>Pescatarian</h3>
-                    </label>
-                    <label className={styles.answer}>
-                        <input
-                            type="radio"
-                            name="answer"
-                            value="ovo vegetarian"
-                            checked={answer === 'ovo vegetarian'}
-                            onChange={handleAnswer}
-                        />
-                        <h3>Ovo Vegetarian</h3>
-                    </label>
-                    <label className={styles.answer}>
-                        <input
-                            type="radio"
-                            name="answer"
-                            value="ovo-lacto vegetarian"
-                            checked={answer === 'ovo-lacto vegetarian'}
-                            onChange={handleAnswer}
-                        />
-                        <h3>Ovo-Lacto Vegetarian</h3>
-                    </label>
+                    <div className={styles.answerBox}  style={{
+                            backgroundColor: handleLableTwo()
+                        }}>
+                        <label className={styles.answer}>
+                            <Image src={data[0].img[1]} width={125} height={125}/>
+                            <input
+                                type="radio"
+                                name="answer"
+                                value="pescatarian"
+                                checked={answer === 'pescatarian'}
+                                onChange={handleAnswer}
+                            />
+                            <h3>{data[0].options[1]}</h3>
+                        </label>
+                    </div>
+                    <div className={styles.answerBox}  style={{
+                            backgroundColor: handleLableThree()
+                        }}>
+                        <label className={styles.answer}>
+                            <Image src={data[0].img[2]} width={125} height={125}/>
+                            <input
+                                type="radio"
+                                name="answer"
+                                value="ovo vegetarian"
+                                checked={answer === 'ovo vegetarian'}
+                                onChange={handleAnswer}
+                            />
+                            <h3>{data[0].options[2]}</h3>
+                        </label>
+                    </div>
+                    <div className={styles.answerBox}  style={{
+                            backgroundColor: handleLableFour()
+                        }}>
+                        <label className={styles.answer}>
+                            <Image src={data[0].img[3]} width={125} height={125}/>
+                            <input
+                                type="radio"
+                                name="answer"
+                                value="ovo-lacto vegetarian"
+                                checked={answer === 'ovo-lacto vegetarian'}
+                                onChange={handleAnswer}
+                            />
+                            <h3>{data[0].options[3]}</h3>
+                        </label>
+                    </div>
                 </div>
-                <button hidden={!answer}>Next</button>
+                <button className={styles.btn} hidden={!answer} onClick={() => setIsActive(false)}>Confirm</button>
             </div>
-        </>
+        </div>
     )
 }
--- components/QuestionBoxThree/index.js
+++ components/QuestionBoxThree/index.js
@@ -1,7 +1,73 @@
 import styles from './QuestionBoxThree.module.css'
+import { useState, useEffect } from 'react'
+import Image from 'next/image'
+import { questions } from '@/data/questions.js'
 
 export default function QuestionBoxThree() {
+    const [answer, setAnswer] = useState('')
+    const [isActive, setIsActive] = useState(false);
+    const [data, setData] = useState(questions);
+    
+    const handleAnswer = (event) => {
+        setAnswer(event.target.value);
+    }
+
+    const handleLableOne = () => {
+        if(answer === 'breakfast'){
+            return 'var(--fern-green)';
+        }
+    }
+
+    const handleLableTwo = () => {
+        if(answer === 'dinner/lunch'){
+            return 'var(--fern-green)';
+        }
+    }
+
     return (
-        <></>
+        <div className={styles.container}>
+        <div className={styles.progressBar}>
+                <div className={styles.progressDone}></div>
+                <div className={styles.progressDone}></div>
+                <div className={styles.progressDone}></div>
+            </div>
+
+            <div className={styles.quizContainer}>
+                <h1 className={styles.question}>{data[2].question}</h1>
+                <div className={styles.answerCont}>
+                    <div className={styles.answerBox}  style={{
+                            backgroundColor: handleLableOne()
+                        }}>
+                        <label className={styles.answer}>
+                            <Image src={data[2].img[0]} width={125} height={125}/>
+                            <input
+                                type="radio"
+                                name="answer"
+                                value="breakfast"
+                                checked={answer === 'breakfast'}
+                                onChange={handleAnswer}
+                            />
+                            <h3>{data[2].options[0]}</h3>
+                        </label>
+                    </div>
+                    <div className={styles.answerBox}  style={{
+                            backgroundColor: handleLableTwo()
+                        }}>
+                        <label className={styles.answer}>
+                            <Image src={data[2].img[1]} width={125} height={125}/>
+                            <input
+                                type="radio"
+                                name="answer"
+                                value="dinner/lunch"
+                                checked={answer === 'dinner/lunch'}
+                                onChange={handleAnswer}
+                            />
+                            <h3>{data[2].options[1]}</h3>
+                        </label>
+                    </div>
+                </div>
+                <button className={styles.btn} hidden={!answer} onClick={() => console.log(answer)}>Confirm</button>
+            </div>
+        </div>
     )
 }
