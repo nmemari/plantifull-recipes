a20ca4284237c8d5ca00c6c5559d4d9980a09853
Navid Memari <work.nmemari@gmail.com>
2023-04-12 20:02:25 -0700

    size adjustments

--- components/BeginBox/index.js
+++ components/BeginBox/index.js
@@ -5,11 +5,11 @@ export default function BeginBox() {
     return (
         <div className={styles.container}>
             <div className={styles.beginBox}>
-                <h6>We want to get to know you</h6>
+                <h1>We want to get to know you</h1>
                 <p>take our quiz to find the recipe that's best for you.</p>                
             </div>
-            <Link className={styles.btn} href='tkquiz'>Take the Quiz</Link>
-            <Link className={styles.btn} href='tutebtn'>Tutorial</Link>
+            <Link className={styles.btn} href='quiz'>Take the Quiz</Link>
+            <Link className={styles.btn} href='tutorial'>Tutorial</Link>
         </div>
     )
 }
