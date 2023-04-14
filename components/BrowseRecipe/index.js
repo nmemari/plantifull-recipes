ded6475a056fab945b098b70c0e8a940ce6bb4aa
tham346 <thamashamara003@gmail.com>
2023-04-14 14:25:59 -0700

    browse page

---
+++ components/BrowseRecipe/index.js
@@ -0,0 +1,23 @@
+import styles from './BrowseRecipe.module.css'
+import Router from 'next/router';
+import { useState } from 'react';
+
+export default function BrowseRecipe( {
+    title='',
+    tag='',
+    tag2='',
+    tag3='',
+    func={}
+}) {
+    
+
+    return (
+        <div>
+        <div>
+            <div>{title}</div>
+            <div>{}</div>
+        </div>
+
+    </div>
+    )
+    }
