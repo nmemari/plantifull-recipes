d01d1ea30c680e60d02367aacc10084a2cb5faeb
tham346 <thamashamara003@gmail.com>
2023-04-14 10:26:21 -0700

    changes

---
+++ components/BrowseRecipe/index.js
@@ -0,0 +1,14 @@
+import styles from './BrowseRecipe.module.css'
+
+
+export default function BrowseRecipe() {
+    return (
+        <div className={styles.container}>
+            <h3>Browse Recipe</h3> 
+            <div className={styles.browsebox1}>
+                
+            </div>
+           
+        </div>
+    )
+} 
---
+++ pages/browse.js
@@ -0,0 +1,7 @@
+import BrowseRecipe from "@/components/BrowseRecipe"
+import Image from "next/image"
+import { useState } from 'react'
+import { useRouter } from "next/router"
+import { recipe } from "@/data/recipe"
+import Link from "next/link"
+
